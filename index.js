5418736e17103ae22791f3882793eed640ee1044
Antoine F Moevus <2613844+antmoev@users.noreply.github.com>
2025-04-08 19:31:07 -0400

    Comment debug message

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_SERVER}`;
+//console.log(uri)
+
 mongoose.connect(uri)
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(err => console.error('Erreur de connexion à MongoDB :', err));
 
 
-console.log(uri)
