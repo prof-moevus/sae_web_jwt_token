1995a90605cda8305f4c4e98752d5357be906606
Antoine F Moevus <2613844+antmoev@users.noreply.github.com>
2025-04-08 19:20:38 -0400

    Add connection to MongoDB Atlas

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
 
 require('dotenv').config();
 
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_SERVER}`;
+mongoose.connect(uri)
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(err => console.error('Erreur de connexion à MongoDB :', err));
+
 
 console.log(uri)
