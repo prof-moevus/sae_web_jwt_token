78c2099161ab9e99cae32448005cc225a70d476c
Antoine F Moevus <2613844+antmoev@users.noreply.github.com>
2025-04-15 19:58:50 -0400

    Add code for a dynamic clock

--- public/js/exo2.js
+++ public/js/exo2.js
@@ -1,9 +1,30 @@
-$("#heure").text((new Date()).getHours());
+date_echeance = null;
+date_courante = null;
+
 $("#heures").text((new Date()).getHours());
 $("#minutes").text((new Date()).getMinutes());
 $("#secondes").text((new Date()).getSeconds());
 
-console.log($('a').attr('href'));
-$('a').attr('href',  "/mon-site-de-hacker-allo")
+function update(){
+    updateTime()
+    updateDisplay()
+}
 
-$("#countdown-timer").before('<p>Contenu ajouté à la fin</p>');
+function updateTime(){
+const heures = (new Date()).getHours();
+const minutes = (new Date()).getMinutes();
+const secondes = (new Date()).getSeconds();
+
+    $("#heures").text(heures);
+    $("#minutes").text(minutes);
+    $("#secondes").text(secondes);
+
+}
+
+function updateDisplay(){
+    $("#heures-sep").toggleClass("sep-display")
+    $("#minutes-sep").toggleClass("sep-display")
+
+}
+
+setInterval(update, 500);
